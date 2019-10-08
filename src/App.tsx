76a8863f0e4d544a34658aa9d1660123ebc4850b
rlmcneary2 <rich@bitovi.com>
2019-10-08 11:54:39 -0400

    Use latest Marker.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Layer, Map, Marker, Popup } from "remapgl";
 import styled from "@emotion/styled";
 import { LayerProps } from "remapgl/dist/layer/layer";
+import { LngLat } from "remapgl/dist/types/location";
+import { MarkerProps } from "remapgl/dist/marker/marker-types";
 
 const App: React.FC = () => {
   const [layers, setLayers] = React.useState(layerData);
-  React.useEffect(() => {
-    setTimeout(() => {
-      const [a, b] = layers;
-      setLayers([b, a]);
-    }, 3000);
-  }, [layers]);
+  // React.useEffect(() => {
+  //   setTimeout(() => {
+  //     const [a, b] = layers;
+  //     setLayers([b, a]);
+  //   }, 3000);
+  // }, [layers]);
 
   const [location, setLocation] = React.useState<{ center: [number, number]; zoom: number; }>({ center: [-68.2954881, 44.3420759], zoom: 9.5 });
+  const [markers, setMarkers] = React.useState<Partial<MarkerProps>[]>([]);
   const centers = React.useRef<[number, number][]>([[-68.8008887, 44.5591077], [-68.5923347, 44.434114], [-68.2954881, 44.3420759]]);
-  React.useEffect(() => {
-    setTimeout(() => {
-      const [a, ...rest] = centers.current;
-      centers.current = [...rest, a];
-      setLocation({
-        center: a,
-        zoom: location.zoom === 9.5 ? 11 : 9.5
-      });
-    }, 3500);
-  }, [location]);
+  // React.useEffect(() => {
+  //   setTimeout(() => {
+  //     const [a, ...rest] = centers.current;
+  //     centers.current = [...rest, a];
+  //     setLocation({
+  //       center: a,
+  //       zoom: location.zoom === 9.5 ? 11 : 9.5
+  //     });
+  //   }, 3500);
+  // }, [location]);
 
   const { center, zoom } = location;
 
@@ -32,13 +35,12 @@ const App: React.FC = () => {
       accessToken="pk.eyJ1IjoicmxtY25lYXJ5MiIsImEiOiJjajgyZjJuMDAyajJrMndzNmJqZDFucTIzIn0.BYE_k7mYhhVCdLckWeTg0g"
       center={center}
       motionType="ease"
+      onClick={({ lngLat }) => setMarkers([...markers, { draggable: true, location: lngLat, popup: createPopup }])}
       zoom={zoom}
     >
-      <Marker
-        location={center}
-        popup={() => <Popup closeButton offset={100}>I'm a popup</Popup>}
-        showPopup={[ "click", "hover" ]}
-      />
+      {markers.map(x => (
+        createMarker(x)
+      ))}
       {layers.map(x => (
         <Layer key={x.id} {...x} />
       ))}
@@ -52,6 +54,30 @@ const MapStyled = styled(Map)`
   height: 100vh;
   width: 100vw;
 `;
+
+function ClickToggleMarker(props: MarkerProps): JSX.Element {
+  const [popup, setPopup] = useState(false);
+  return (<Marker {...props} onClick={() => setPopup(!popup)} togglePopup={popup} />);
+}
+
+function createMarker(props: Partial<MarkerProps>): JSX.Element | null {
+  const { location, ...otherProps } = props;
+  if (!location) {
+    return null;
+  }
+
+  return (
+    <ClickToggleMarker
+      key={Array.isArray(location) ? `${location[0]}${location[1]}` : `${location.lat}${location.lat}`}
+      {...otherProps}
+      location={location}
+    />
+  );
+}
+
+function createPopup() {
+  return <Popup offset={40}>I'm a popup.</Popup>
+}
 
 const data = {
   "features": [
@@ -162,7 +188,6 @@ const data = {
   ],
   "type": "FeatureCollection"
 };
-
 
 const paint = {
   "circle-color": "#222",
